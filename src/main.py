1fd5908c767faf536f0d7446927468d06ddd4f5d
IsCowdell <isabella.cowdell@ucas-edu.net>
2026-02-19 09:30:03 -0700

    mian menu changes

--- src/main.py
+++ src/main.py
@@ -1,19 +1,20 @@
 #This is the main for the user interface
 
-# 
-#ask the user are you a new user or do you want to sign in 
-# if user is new user then play set up function 
-#   else if old user then play old user function 
-# else:
-#       incoorect input and show user Lets try that again
-# after all of that send them to where we ask 
-# asking user what game do you guys want to play or if they want to sign out 
-#  if user says to play number gessing game
-# then play the function for number guessing game
-#   else if if user wants the tic tac toe 
-#   then play the tick tac toe function 
-#   else if user wants to quit then
-#   Show the user thank you for using the program goodbye
+# User athefcation thingy
+
+# asking user if they want to play tick toe ornumbers guessing game or view high scores or quit 
+# tick tack toe option is 1 
+# guessing game is 2
+# view high score is 3 
+# quit is 4 
+#  if user choice is 1
+#      then play the function for number guessing game
+#   # else if user choice is 2
+#       then play the tick tac toe function 
+#   # else if user choice is 3 
+        #play the high score function 
+      #  if user choice is 4
+        #then have them quit 
 #   else:
         #show the user incorrect input try again
         #send the user back up 
