50926f915ddff50cd82606c728e2c87e4b0a9f11
IsCowdell <isabella.cowdell@ucas-edu.net>
2026-02-19 09:23:21 -0700

    Finished main menu

--- src/main.py
+++ src/main.py
@@ -1,1 +1,20 @@
 #This is the main for the user interface
+
+# 
+#ask the user are you a new user or do you want to sign in 
+# if user is new user then play set up function 
+#   else if old user then play old user function 
+# else:
+#       incoorect input and show user Lets try that again
+# after all of that send them to where we ask 
+# asking user what game do you guys want to play or if they want to sign out 
+#  if user says to play number gessing game
+# then play the function for number guessing game
+#   else if if user wants the tic tac toe 
+#   then play the tick tac toe function 
+#   else if user wants to quit then
+#   Show the user thank you for using the program goodbye
+#   else:
+        #show the user incorrect input try again
+        #send the user back up 
+        
