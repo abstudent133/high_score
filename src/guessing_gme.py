0f66ed1a54bca9fa2de3dfcc385c801a708347eb
IsCowdell <isabella.cowdell@ucas-edu.net>
2026-02-19 09:48:03 -0700

    guessing game branch

---
+++ src/guessing_gme.py
@@ -0,0 +1,13 @@
+#IC 1st guessing number game 
+#import random 
+# show the user hello you have entered the numbers guessing game 
+# the point of this game is to guess a number between 1 - 1000
+# quit is equal to false 
+# while quit is equal to false let the game contiune 
+#   get a random number in the range of 1 - 1000
+#   show the user im thinking abt a number from 1 - 1000 how abt you guess
+#   asking the user guess a number bewtween 1 - 1000 
+#   save this as a variable under the name guess 
+#   if user guess is exact then add ten to score 
+#       else if guess is 10 
+#         
