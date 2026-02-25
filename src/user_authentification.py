f47f1aff0bcbec18e953b70bdc470ec088756fbc
lulu094 <lucci.venanciomostaceli@ucas-edu.net>
2026-02-25 09:33:22 -0700

    did some coding for sign in

--- src/user_authentification.py
+++ src/user_authentification.py
@@ -17,6 +17,36 @@
 # Print "Try again? (yes / no)"
 # Store input as choice
 
+def sign_in():
+    while True:
+        username = input("Enter your username: ")
+        password = input("Enter your password: ")
+        # Open csv and check if username and password match
+        csv_file = open("user_info.csv", "r") # going to change the name of the csv file once we have it
+        csv_reader = csv.reader(csv_file)
+        match = False
+        for row in csv_reader:
+            if row[0] == username and row[1] == password:
+                match = True
+                break
+        csv_file.close()
+        # If they do, print "You've succesfully signed in" and break loop
+        if match:
+            print("You've succesfully signed in")
+            break
+        # Else, print "Wrong username or password input" and ask "Try again? (yes / no)"
+        # Store input as choice
+        else:
+            print("Wrong username or password input")
+            choice = input("Try again? (yes / no): ")
+        # If choice is no, exit program
+        if choice == "no":
+            exit() # main() or exit()
+        # Else, continue loop to try again
+        else:
+            continue
+# Test code later once I have the CSV file
+
 # - All long information stored in separate file
 
 # - Log out => Takes them to log in screen where they have the option to end program
