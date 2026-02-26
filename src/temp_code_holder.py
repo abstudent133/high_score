bf7f49318501fc372ce50c3d2fa082033447c648
lulu094 <lucci.venanciomostaceli@ucas-edu.net>
2026-02-26 09:33:27 -0700

    main menu

--- src/temp_code_holder.py
+++ src/temp_code_holder.py
@@ -1,60 +1,53 @@
 # File to hold code being worked on so that there aren't merge issues
 
-# PASSWORD REQUIREMENTS
-# length is >= 12
-# Contains at least one number
-# contains at least one UPPERCASE letter
-# Contains at least one lowercase letter
-# Contains at least one special character (maybe a tuple of all special characters that it compares to)
-def pass_requirements(password):
-    score = 0
-    special_charcters = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "}", "[", "]", "|", "\\", ":", ";", '"', "'", "<", ">", ",", ".", "?", "/"]
-    uppercase = any(char.isupper() for char in password)
-    lowercase = any(char.islower() for char in password)
-    digit = any(char.isdigit() for char in password)
-    # Getting requirements
-    if len(password) >= 12:
-        score += 1
-    if uppercase == True:
-        score += 1
-    if lowercase == True:
-        score += 1
-    if digit == True:
-        score += 1
-    for char in password:
-        if char in special_charcters:
-            score +=1 
-            break
-    # Requirments aquired. Now check if the score is a certain value. Return true if it is
-    if score >= 5:
-        return True
-    else:
-        return False
-    
+# LV 1st User Authentification
+import csv
 
-# Go through the csv and check if username the user gave is NOT equal to any already in there (this means that the user given username is unique and avaliable for use)
-# Once user has a valid username, do a while loop for the password.
-# this function will take in two parameters: the string it is checking for and which column (ex: pass in username and column 0 when checking if username avaliable and password + 1 for checking if password avliable)
-def item_avaliable(string, column, key):
-    # I need CSV to work on this
-    # First, if column == 0, this is USERNAME
-    # If column == 1, this is PASSWORD and I need to access the key to hash the given string BEFORE comparing
-    # Open file first
-    # If column == 0:
-        # for line in content:
-            # if string != line[0]
-                # continue
-            # else:
-                # the username already made matches the given string. Not a valid username
-                # break
-    # elif column == 1:
-        # for line in content:
-            # the_key = line[2]
-            # if string(that has been hased by the_key) != line[1]
-                # continue
-            # else:
-                # the password already made matches the given string. Not a valid password
-                # break
-    # else:
-        # What in God's name did you do to my code?!?!?!?!? Column should only be 1 or 2
-    pass
+# Requirements 
+
+# - Find out if new or old user
+# Ask user to enter username
+# Store info as username
+# Ask user to enter password
+# Store info as password
+
+# - Verify user logged in
+# If username and password match
+# Print "You've succesfully signed in"
+# Exit loop (take them to the game/signed in menu)
+# Else
+# Print "Wrong username or password input"
+# Print "Try again? (yes / no)"
+# Store input as choice
+
+def sign_in():
+    while True:
+        username = input("Enter your username: ")
+        password = input("Enter your password: ")
+        # Open csv and check if username and password match
+        csv_file = open("user_info.csv", "r") # going to change the name of the csv file once we have it
+        csv_reader = csv.reader(csv_file)
+        match = False
+        for row in csv_reader:
+            if row[0] == username and row[1] == password:
+                match = True
+                break
+        csv_file.close()
+        # If they do, print "You've succesfully signed in" and break loop
+        if match:
+            print("You've succesfully signed in")
+            break
+        # Else, print "Wrong username or password input" and ask "Try again? (yes / no)"
+        # Store input as choice
+        else:
+            print("Wrong username or password input")
+            choice = input("Try again? (yes / no): ")
+        # If choice is no, exit program
+        if choice == "no":
+            exit() # main() or exit()
+        # Else, continue loop to try again
+        else:
+            continue
+# Test code later once I have the CSV file
+
+# - All long information stored in separate file
