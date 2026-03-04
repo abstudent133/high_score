3bbaf54a4587611d744604c38cd0b29fd5fd57f0
IsCowdell <isabella.cowdell@ucas-edu.net>
2026-03-04 09:27:45 -0700

    guessing game fixed error

--- src/guessing_gme.py
+++ src/guessing_gme.py
@@ -9,15 +9,15 @@ def guessing_game():
     # the point of this game is to guess a number between 1 - 500
     print("the point of this game is to guess a number between 1- 500")
     # quit is equal to false 
-    quit == False
+    quit = False
     # while quit is equal to false let the game contiune 
-    while quit != True: 
+    while quit == False: 
     #   get a random number in the range of 1 - 500
         random_number = random.randint(1,201)
     #   show the user im thinking abt a number from 1 - 500 how abt you guess
-        print("Im thinking about a number from 1 - 500:")
+        print("Im thinking about a number from 1 - 200:")
     #   asking the user guess a number bewtween 1 - 500 
-        guess_input = input("guess a number between 1-500:")
+        guess_input = input("guess a number between 1-200:")
     #   save this as a variable under the name guess 
         guess = int(guess_input)
         if guess == random_number:
@@ -37,38 +37,39 @@ def guessing_game():
             # if (guess >= random_number - 30 and guess < random_number) or (guess <= random_number + 30 and guess > random_number)
         elif (guess >= random_number - 30 and guess < random_number) or (guess <= random_number + 30 and guess > random_number):
     #         # add three to score
-            score += 5 
+            score += 3
             print(f"your score is {score}")
             # if (guess >= random_num - 40 and guess < random_num) or (guess <= random_num + 40 and guess > random_num)
         elif (guess >= random_number - 40 and guess < random_number) or (guess <= random_number + 40 and guess > random_number):
                 # add two to score
             score += 2
             # if (guess >= random_num - 50 and guess < random_num) or (guess <= random_num + 50 and guess > random_num)
+            print(f"your score is {score}")
         elif (guess >= random_number - 50 and guess < random_number) or (guess <= random_number + 50 and guess > random_number):
                 # add one to score
             score += 1
+            print(f"your score is {score}")
             #else:
         else:
                 #show the user you got no points
             print("you got no points")
         # ask user if they want to play again (yes/no)
-        input("do you want to play again yes or no:")
+        again = input("do you want to play again yes or no:").strip().lower()
             # if yes
-        if "yes":
+        if again == "yes":
                         # quit equals true
-                    quit == False
+            quit = False
                     
-                    continue
+            continue
                     # else:
-        elif "no":
-              print("goodbye")
-              quit == True
-              break
+        elif again == "no":
+            print("goodbye")
+            quit = True
+            break
         else:
-               print("incorrect input")
-    else:
-        quit == True 
-        print("goodbye")
+            print("incorrect input")
+    
+    return score
 
     # at end, send back to signed in menu   
     quit == True
---
+++ src/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+
+        guess = int(guess_input)
