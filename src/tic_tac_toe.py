3f4ae28ad8fa43f1b3b9e883b6e684d6d73c1ad7
abstudent133 <anna.bangerter@ucas-edu.net>
2026-03-06 05:29:52 +0000

    fixed little thing in tic tac toe

--- src/tic_tac_toe.py
+++ src/tic_tac_toe.py
@@ -87,7 +87,7 @@ def play_tic_tac_toe():
                 win = False
             elif board[0][2] == "O" and board[1][1] == "O" and board[2][0] == "O":
                 win = False
-            elif board[0] != [" "," "," "] and board[1] != [" "," "," "] and board[2] != [" "," "," "]:
+            elif " " not in board[0] and " " not in board[1] and " " not in board[2]:
                 win = False
             else:
                 continue
